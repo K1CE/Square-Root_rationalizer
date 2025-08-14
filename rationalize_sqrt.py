88095b518c9c0e8e527977da52a1642ddd148e2e
K1CE <6kouman@gmail.com>
2025-08-13 23:50:46 -0700

    docs: fix grammer mistake

--- rationalize_sqrt.py
+++ rationalize_sqrt.py
@@ -1,5 +1,5 @@
 
-print("\n\n===== sqrt rationalizer ========\n     leave blank for default\n     This program is designed to find the multiple of a square root which creates the closest result to a whole number\n     The original purpose of this was to find side lengths of right triangles and other geometric shapes which approximate to workable lengths.\n     √2 or √3 is the usual problem number\n\n")
+print("\n\n===== sqrt rationalizer ========\n     leave blank for default\n     This program is designed to find the multiples of a square root which create the closest result to a whole number\n     The original purpose of this was to find side lengths of right triangles and other geometric shapes which approximate to workable lengths.\n     √2 or √3 is the usual problem number\n\n")
 user_radicand = input(">enter radicand: √")
 user_limit = input(">enter upper limit (default is 20): ") 
 user_increment = input(">enter allowed increment (default is 1): ")
